11d9ea8c96fb408293de1f616cb4ff2fbff997a0
mejjete <dani1.mlp1@gmail.com>
2020-05-25 03:47:44 -0700

    (feat) added vstl::function

--- include/vfunctional.hpp
+++ include/vfunctional.hpp
@@ -3,6 +3,9 @@
 #include "vbinder.hpp"
 #include "vpredicate.hpp"
 #include "vinterface.hpp"
+#include <memory>
+#include <cstdlib>
+#include <cassert>
 namespace vstl
 {
     //Binders
@@ -16,6 +19,45 @@ namespace vstl
     binder2nd<Operation> bind2nd(const Operation& op, const T& x)
     {
         return binder2nd<Operation>(op, typename Operation::second_argument_type(x));
+    };
+    
+    template <typename T>
+    class function;
+
+    template <typename ReturnValue, typename ...Args>
+    class function<ReturnValue(Args...)>
+    {
+        public:
+            template <typename T>
+            function& operator=(T t)
+            {
+                callable_ = std::make_unique<CallableT<T>>(t);
+                return *this;
+            };
+            ReturnValue operator()(Args... argc) const 
+            {
+                assert(callable_);
+                return callable_->Invoke(argc...);
+            };
+        private:
+            class iCallable
+            {
+                public:
+                    virtual ~iCallable() = default;
+                    virtual ReturnValue Invoke(Args...) = 0;
+            };
+
+            template <typename T>
+            class CallableT : public iCallable
+            {
+                public:
+                    CallableT(const T& t): t_(t) {};
+                    virtual ~CallableT() override = default;
+                    ReturnValue Invoke(Args... args) override { return t_(args...); };
+                private:
+                    T t_;
+            };
+            std::unique_ptr<iCallable> callable_;
     };
 }
 #endif
