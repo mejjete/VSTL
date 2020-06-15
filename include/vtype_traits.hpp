9835bc592992fddb02f49743907df6f4038c391f
mejjete <dani1.mlp1@gmail.com>
2020-06-15 01:05:05 -0700

    (feat) added add_rvalue_reference and add_lvalue_reference

--- include/vtype_traits.hpp
+++ include/vtype_traits.hpp
@@ -12,6 +12,7 @@ namespace vstl
     #define VSTL_TYPE_RELATIONSHIP
     #define VSTL_MISCELLANIOUS_TRANSFORMATION
     #define VSTL_CONST_VOLATILE_MODIFICATION
+    #define VSTL_REFERENCE_MODIFICATION
  
     namespace detail
     {
@@ -20,6 +21,21 @@ namespace vstl
         struct two { char c[2]; };
         template <typename T>
         two test(...);
+
+        template <typename T>
+        struct type_identity { using type = T; };
+
+        template <typename T>
+        auto try_add_lvalue_reference(int) -> type_identity<T&>;
+
+        template <typename T>
+        auto try_add_lvalue_reference(...) -> type_identity<T>;
+
+        template <typename T>
+        auto try_add_rvalue_reference(int) -> type_identity<T&&>;
+
+        template <typename T>
+        auto try_add_rvalue_reference(...) -> type_identity<T>;
     }
 
     template <typename T, T v>
@@ -33,6 +49,46 @@ namespace vstl
 
     typedef integral_constant<bool, true>   true_type;
     typedef integral_constant<bool, false>  false_type;
+
+    #ifdef VSTL_REFERENCE_MODIFICATION
+
+    //remove reference
+    template <typename T>
+    struct remove_reference
+    {
+        typedef T type;
+    };
+
+    template <typename T>
+    struct remove_reference<T&>
+    {
+        typedef T type;
+    };
+
+    template <typename T>
+    struct remove_reference<T&&>
+    {
+        typedef T type;
+    };
+
+    template <typename T>
+    using remove_reference_t = typename vstl::remove_reference<T>::type;
+
+    //add lvalue reference 
+    template <typename T>
+    struct add_lvalue_reference : decltype(detail::try_add_lvalue_reference<T>(0)) {};
+
+    template <typename T>
+    using add_lvalue_reference_t = typename vstl::add_lvalue_reference<T>::type;
+
+    //add rvalue reference 
+    template <typename T>
+    struct add_rvalue_reference : decltype(detail::try_add_rvalue_reference<T>(0)) {};
+
+    template <typename T>
+    using add_rvalue_reference_t = typename vstl::add_rvalue_reference<T>::type;
+
+    #endif
 
     #ifdef VSTL_CONST_VOLATILE_MODIFICATION
 
