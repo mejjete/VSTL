b561fcc8f12264f4135c5f9873a2b1c5717b8eff
mejjete <danil.malapura@gmail.com>
2021-07-21 18:26:00 +0300

    added detection idiom features

--- include/vdetail/type_traits/libc_traits.hpp
+++ include/vdetail/type_traits/libc_traits.hpp
@@ -20,6 +20,49 @@ namespace vstl
     */
     #define VSTL_NESTED_TYPE(_TYPE, _SUBTYPE)   \
     __has_##_SUBTYPE<_TYPE>::value
+
+        //primary template
+    //negative case 
+    template <  typename Default, class, 
+                template<typename...> class Op,
+                typename... Args>
+    struct detector 
+    {
+        constexpr static bool value = false;
+        using type = Default;
+    };
+
+
+    //specialization recognizes and handles only types supporting Op
+    //positive case
+    template <  typename Default, 
+                template<typename...> class Op,
+                typename... Args>
+    struct detector<Default, __void_t<Op<Args...>>, Op, Args...>
+    {
+        constexpr static bool value = true;
+        using type = Op<Args...>;
+    };
+
+
+    template <template<typename...> class Op, typename... Args>
+    using is_detected = detector<void, void, Op, Args...>;
+
+
+    template <template<typename...> class Op, typename... Args>
+    using is_detected_v = typename is_detected<Op, Args...>::value;
+
+
+    template <template<typename...> class Op, typename... Args>
+    using detected_t = typename is_detected<Op, Args...>::type;
+
+
+    template <typename Default, template <typename...> class Op, typename... Args>
+    using detected_or = detector<Default, void, Op, Args...>;
+
+
+    template <typename Default, template <typename...> class Op, typename... Args>
+    using detected_or_t = typename detected_or<Default, Op, Args...>::type;
 };
 
 #endif 
